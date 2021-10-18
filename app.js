da31b3598d5d079ee0873699aece61594b6a6426
sholabioku <bilushonline@gmail.com>
2021-10-18 20:56:38 +0100

    Require axios

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 app.get('/api/ping', (req, res) => {
   res.status(200).json({ success: true });
 });
+
+// app.get('/api/posts', async (req, res) => {});
 
 const port = process.env.PORT || 3000;
 
