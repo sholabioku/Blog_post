7923a20b2222a07718c52c23ac3f5029a92f2aba
sholabioku <bilushonline@gmail.com>
2021-10-18 20:51:19 +0100

    Add GET /api/ping route and its functionality

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/api/ping', (req, res) => {
+  res.status(200).json({ success: true });
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
