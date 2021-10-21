44592bd5af926620411dca55029add40b12f41d2
sholabioku <bilushonline@gmail.com>
2021-10-21 12:08:57 +0100

    Refracture code

--- app.js
+++ app.js
@@ -2,14 +2,9 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-app.get('/api/ping', (req, res) => {
-  res.status(200).json({ success: true });
-});
+const AppController = require('./src/controller');
 
-// app.get('/api/posts', async (req, res) => {});
-
+app.get('/api/ping', AppController.home);
+app.get('/api/posts', AppController.post);
 const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+app.listen(port, () => console.log(`App listening on port ${port}`));
